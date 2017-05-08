a806fe40fd3696c754ecc5e73993116eafabec4e
Zach Whitehead <zachmanw@gmail.com>
2017-05-08 11:19:20 -0400

    Add led error pallet and initial haptic support

--- eppg-controller.ino
+++ eppg-controller.ino
@@ -4,13 +4,14 @@
 #include <FastLED.h> //For RGB lights
 
 // Arduino Pins
-#define LED_SW        9 // output for LED on button switch
-#define ESC_PIN       8 // the ESC signal output pin 
-#define THROTTLE_PIN  A0 // throttle pot input 
+#define THROTTLE_PIN  A0 // throttle pot input
+#define HAPTIC_PIN    3 // Vibration motor
 #define LED_PIN       5 // LED strip
+#define LED_SW        8 // output for LED on button switch 
+#define ESC_PIN       9 // the ESC signal output 
 
 #define NUM_LEDS    10
-#define BRIGHTNESS  64
+#define BRIGHTNESS  200
 #define LED_TYPE    WS2811
 #define COLOR_ORDER GRB
 
@@ -19,24 +20,37 @@ Servo esc; //Creating a servo class with name as esc
 
 const long interval = 750;   // interval at which to blink (milliseconds)
 unsigned long previousMillis = 0;     // will store last time LED was updated
+
+CRGBPalette16 currentPalette;
+TBlendType    currentBlending;
+
 void setup()
 {
   delay( 1500 ); // power-up safety delay
+  Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT); //onboard LED
   pinMode(LED_SW, OUTPUT); //setup the external LED pin
+  pinMode(HAPTIC_PIN, OUTPUT); 
   FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
   FastLED.setBrightness(  BRIGHTNESS );
+
+  currentBlending = LINEARBLEND;
+  SetupBlackAndRedStripedPalette();
   
   esc.attach(ESC_PIN);
-
-  Serial.begin(9600);
   
   checkArmRange();
   // Arming range check exited so continue
   digitalWrite(LED_SW, HIGH);
   digitalWrite(LED_BUILTIN, HIGH);
+  
+  analogWrite(HAPTIC_PIN, 200);
+  delay( 1500 ); // TODO move to timer with callback
+  analogWrite(HAPTIC_PIN, 0);
+  
   // TODO indicate armed on LED strip
   esc.writeMicroseconds(1000); //initialize the signal to 1000
+
 }
 
 void checkArmRange(){
@@ -70,11 +84,43 @@ void checkArmRange(){
 
 void loop()
 {
+  static uint8_t startIndex = 0;
+  startIndex = startIndex + 1; /* motion speed */
+    
+  FillLEDsFromPaletteColors( startIndex);
+  FastLED.show();
+  //FastLED.delay(20);
+
   // TODO check and display battery voltage
   int val; 
   val= analogRead(THROTTLE_PIN);
   
-  val= map(val, 0, 1023,1000,2000); //mapping val to minimum and maximum(Change if needed) 
+  val= map(val, 0, 1023,1000,2000); //mapping val to minimum and maximum 
   esc.writeMicroseconds(val); //using val as the signal to esc
+}
+
+void FillLEDsFromPaletteColors( uint8_t colorIndex)
+{
+    uint8_t brightness = 255;
+    
+    for( int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
+        colorIndex += 3;
+    }
+}
+
+void SetupBlackAndRedStripedPalette()
+{
+    // 'black out' all 16 palette entries...
+    fill_solid( currentPalette, 16, CRGB::Black);
+    // and set every fourth one to white.
+    currentPalette[0] = CRGB::Red;
+    currentPalette[2] = CRGB::Orange;
+    currentPalette[4] = CRGB::Red;
+    currentPalette[6] = CRGB::Orange;
+    currentPalette[8] = CRGB::Red;
+    currentPalette[10] = CRGB::Orange;
+    currentPalette[12] = CRGB::Red;
+    currentPalette[14] = CRGB::Orange;
 }
 
