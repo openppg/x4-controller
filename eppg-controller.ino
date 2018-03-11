8297c0357ce71bce8a845acfc6ebf672be54faa6
Zach Whitehead <zachmanw@gmail.com>
2018-03-11 13:04:32 -0400

    Add screen, arming button, and buzzer pin support

--- eppg-controller.ino
+++ eppg-controller.ino
@@ -1,91 +1,99 @@
-// Zach Whitehead - 2017
+// Zach Whitehead - 2018
 
 #include <Servo.h> //To control ESCs
-#include <FastLED.h> //For RGB lights
 #include <ResponsiveAnalogRead.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+#include <Bounce2.h>
 
 // Arduino Pins
 #define THROTTLE_PIN  A7 // throttle pot input
 #define HAPTIC_PIN    3  // Vibration motor
-#define LED_PIN       5  // LED strip
-#define LED_SW        8  // output for LED on button switch 
+#define LED_PIN       5  // LED on button
+#define BUZZER_PIN    8  // output for LED on button switch 
+#define LED_SW        4  // output for LED on button switch 
 #define ESC_PIN       2  // the ESC signal output 
 #define BATT_IN       A6 //Battery voltage in (5v max)
+#define OLED_RESET    4 // ?
+#define BUTTON_PIN    6 // arm button
 
-#define NUM_LEDS    10
-#define BRIGHTNESS  225
-#define LED_TYPE    WS2811
-#define COLOR_ORDER GRB
+Adafruit_SSD1306 display(OLED_RESET);
 
-CRGB leds[NUM_LEDS];
 Servo esc; //Creating a servo class with name as esc
 
 ResponsiveAnalogRead analog(THROTTLE_PIN, false);
+Bounce debouncer = Bounce();
 
 const long initInterval = 750; // throttle check (milliseconds)
 const long bgInterval = 1500;  // background updates (milliseconds)
 
 unsigned long previousMillis = 0; // will store last time LED was updated
-
-CRGBPalette16 currentPalette;
-TBlendType currentBlending;
-
+int buttonState = 0;
 #pragma message "Warning: OpenPPG software is in beta"
 
 void setup() {
-  delay(1500); // power-up safety delay
+  delay(500); // power-up safety delay
   Serial.begin(9600);
+  
   pinMode(LED_BUILTIN, OUTPUT); //onboard LED
   pinMode(LED_SW, OUTPUT); //setup the external LED pin
-  pinMode(HAPTIC_PIN, OUTPUT);
-  FastLED.addLeds < LED_TYPE, LED_PIN, COLOR_ORDER > (leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-  FastLED.setBrightness(BRIGHTNESS);
+  pinMode(BUTTON_PIN, INPUT);
 
-  currentBlending = LINEARBLEND;
+  // After setting up the button, setup the Bounce instance :
+  debouncer.attach(BUTTON_PIN);
+  debouncer.interval(1000); // interval in ms
+
+  //pinMode(HAPTIC_PIN, OUTPUT);
+  initDisplay();
 
   esc.attach(ESC_PIN);
   esc.writeMicroseconds(0); //make sure off
+  
+  delay(500);
 
-  handleBattery();
-  checkArmRange();
+  //handleBattery();
+  checkArmRange(); 
   // Arming range check exited so continue
-
-  digitalWrite(LED_SW, HIGH);
-  digitalWrite(LED_BUILTIN, HIGH);
 
   // analogWrite(HAPTIC_PIN, 200);
   // delay(1500); // TODO move to timer with callback
   // analogWrite(HAPTIC_PIN, 0);
-
-  Serial.println(F("Sending Arm Signal"));
-  setAllLeds(CRGB::Gray);
-  esc.writeMicroseconds(1000); //initialize the signal to 1000
+  armSystem();
 }
 
 void checkArmRange() {
-  bool throttle_high = true;
+  bool disarmed = true;
   unsigned long currentMillis = millis();
   int ledState = LOW;
   Serial.println(F("Checking throttle"));
-
-  while (throttle_high) {
+  
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("Disarmed");
+  display.display();
+  display.clearDisplay();
+ 
+  while (disarmed) {
     unsigned long currentMillis = millis();
     if (currentMillis - previousMillis >= initInterval) {
       // save the last time throttle checked and LED blinked
 
       previousMillis = currentMillis;
       analog.update();
+      debouncer.update();
+ 
+      buttonState = debouncer.read();
 
-      if (analog.getValue() < 100) {
-        throttle_high = false;
+      if(buttonState == HIGH && analog.getValue() < 100) {
+        disarmed = false;  
       }
 
       // if the LED is off turn it on and vice-versa:
       if (ledState == LOW) {
-        setAllLeds(CRGB::Blue);
         ledState = HIGH;
       } else {
-        setAllLeds(CRGB::Red);
         ledState = LOW;
       }
  
@@ -102,53 +110,85 @@ void loop() {
   if (currentMillis - previousMillis >= bgInterval) {
     // handle background tasks
     previousMillis = currentMillis; // reset
-    handleBattery();
+    //handleBattery();
   }
 }
 
 void handleBattery() {
-  CRGB ledColor;
   int sensorValue = analogRead(BATT_IN);
   float voltage = sensorValue * (5.0 / 1023.0);
-  float percent = mapf(voltage, 4.2, 5, 1, NUM_LEDS);
+  float percent = mapf(voltage, 4.2, 5, 1, 10);
   int numLedsToLight = 1;
   
   if (percent < 0) {percent = 0;}
   
-  int lights = round(percent);
   numLedsToLight = round(percent);
   if (numLedsToLight < 1) {numLedsToLight =1;}
   
-  Serial.print(voltage);
+  //Serial.print(voltage);
 
-  if (numLedsToLight <= 2) {
-    ledColor = CRGB::Red;
-  } else if (numLedsToLight == 3) {
-    ledColor = CRGB::Orange;
-  } else {
-    ledColor = CRGB::Green;
-  }
-  FastLED.clear();
-  for (int led = 0; led < numLedsToLight; led++) {
-    leds[led] = ledColor;
-  }
-  FastLED.show();
+  // TODO display battery info
+  // TODO handle low battery
+}
+
+void initDisplay(){
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
+
+  // Clear the buffer.
+  display.clearDisplay();
+
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("OpenPPG");
+  display.display();
+  display.clearDisplay();
 }
 
 void handleThrottle() {
   analog.update();
   int rawval = analog.getValue();
-  int val = map(rawval, 0, 1023, 1000, 2000); //mapping val to minimum and maximum
+  int val = map(rawval, 0, 1023, 1160, 2000); //mapping val to minimum and maximum
+  Serial.println(val);
   esc.writeMicroseconds(val); //using val as the signal to esc
 }
 
-void setAllLeds(CRGB color) {
-  FastLED.clear();
-  fill_solid(leds, NUM_LEDS, color);
-  FastLED.show();
+void armSystem(){
+  int melody[] = { 1760, 1976, 2093 };
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(LED_SW, HIGH);
+
+  Serial.println(F("Sending Arm Signal"));
+  esc.writeMicroseconds(1000); //initialize the signal to 1000
+
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("Flight");
+  display.println("Mode");
+  display.display();
+  display.clearDisplay();
+
+  digitalWrite(LED_SW, LOW);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  // iterate over the notes of the melody:
+  for (int thisNote = 0; thisNote < 3; thisNote++) {
+
+    //quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000 / 8;
+    tone(BUZZER_PIN, melody[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    delay(noteDuration);
+    // stop the tone playing:
+    noTone(BUZZER_PIN);
+  }
 }
 
 double mapf(double x, double in_min, double in_max, double out_min, double out_max)
 {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
+
