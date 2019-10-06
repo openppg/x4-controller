907fcdd94eb6d60a1b62b57a0144cab594ba56cb
Zach Whitehead <zjwhitehead@users.noreply.github.com>
2019-10-06 14:41:22 -0400

    Add Serial Number Print

--- utilities.ino
+++ utilities.ino
@@ -74,3 +74,20 @@ void printDeviceData() {
   SerialUSB.print("crc ");
   SerialUSB.println(deviceData.crc);
 }
+
+void printChipId() {
+  volatile uint32_t val1, val2, val3, val4;
+  volatile uint32_t *ptr1 = (volatile uint32_t *)0x0080A00C;
+  val1 = *ptr1;
+  volatile uint32_t *ptr = (volatile uint32_t *)0x0080A040;
+  val2 = *ptr;
+  ptr++;
+  val3 = *ptr;
+  ptr++;
+  val4 = *ptr;
+
+  SerialUSB.print("chip id: 0x");
+  char buf[33];
+  sprintf(buf, "%8x%8x%8x%8x", val1, val2, val3, val4);
+  SerialUSB.println(buf);
+}
