19fa268afc8dfc9b5b32d13249efebae9c20d8fd
Zach Whitehead <zjwhitehead@users.noreply.github.com>
2022-04-03 15:21:17 -0500

    More nuanced display refresh vs setup

--- src/sp140/sp140.ino
+++ src/sp140/sp140.ino
@@ -221,6 +221,15 @@ void initButtons() {
 // inital screen setup and config
 void initDisplay() {
   display.initR(INITR_BLACKTAB);  // Init ST7735S chip, black tab
+
+  pinMode(TFT_LITE, OUTPUT);
+  digitalWrite(TFT_LITE, HIGH);  // Backlight on
+  resetDisplay();
+  displayMeta();
+  delay(2000);
+}
+
+void resetDisplay() {
   display.fillScreen(DEFAULT_BG_COLOR);
   display.setTextColor(BLACK);
   display.setCursor(0, 0);
@@ -228,11 +237,6 @@ void initDisplay() {
   display.setTextWrap(true);
 
   display.setRotation(deviceData.screen_rotation);  // 1=right hand, 3=left hand
-
-  pinMode(TFT_LITE, OUTPUT);
-  digitalWrite(TFT_LITE, HIGH);  // Backlight on
-  displayMeta();
-  delay(2000);
 }
 
 // read throttle and send to hub
